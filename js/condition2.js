5817503aa9e327ddfd7644e98d8a756608b3bcbe
fs <px@px.com>
2025-04-25 16:15:29 +0900

    연산자

--- js/condition2.js
+++ js/condition2.js
@@ -1,1 +1,11 @@
 //condition2.js
+
+let num1 = prompt("숫자를 입력하세요!!"); //창실행10
+console.log(num1);
+isTrue=num1 % 2 == 0;
+//짝수 /홀수 판별.
+if (isTrue) {
+  console.log('짝수');
+} else {
+  console.log('홀수');
+}
